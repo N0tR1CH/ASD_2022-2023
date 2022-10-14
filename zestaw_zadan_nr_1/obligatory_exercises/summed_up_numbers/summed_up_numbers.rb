7957c21fb050c4e736179f9e2f5fc59344bf79c3
N0tR1CH <damianrichter58@gmail.com>
2022-10-14 19:56:12 +0200

    create first approach to summed_up_numbers using ruby methods

---
+++ zestaw_zadan_nr_1/obligatory_exercises/summed_up_numbers/summed_up_numbers.rb
@@ -0,0 +1,37 @@
+require 'byebug'
+# adding method to an array class that checks uniqueness of array elements
+class Array
+  def uniq?
+    self == self.uniq
+  end
+end
+
+module FirstApproach
+  # module_function makes method self
+  module_function
+
+  def summing_up_numbers(a)
+    until a.uniq? do
+      sum = a.first; a.shift
+      unless a.find_index(sum).nil?
+        a.delete_at(a.find_index(sum))
+        sum += sum; a.push(sum)
+        next
+      end
+
+      a.push(sum)
+
+      next
+    end
+
+    # returning an array size
+    a.size
+  end
+end
+
+n = gets.chomp.to_i
+a = []
+
+n.times { a << gets.chomp.to_i }
+
+puts "First approach gives length of: #{FirstApproach.summing_up_numbers(a)}."
