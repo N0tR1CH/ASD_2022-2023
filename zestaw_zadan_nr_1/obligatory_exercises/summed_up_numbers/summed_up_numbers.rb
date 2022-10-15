24ddd252d2bb33c599ee3e1973634fd2901f04a6
N0tR1CH <damianrichter58@gmail.com>
2022-10-15 16:02:31 +0200

    refactor first approach for summed up numbers and add spec for it

--- zestaw_zadan_nr_1/obligatory_exercises/summed_up_numbers/summed_up_numbers.rb
+++ zestaw_zadan_nr_1/obligatory_exercises/summed_up_numbers/summed_up_numbers.rb
@@ -1,7 +1,21 @@
+# frozen_string_literal: true
+
 # adding method to an array class that checks uniqueness of array elements
 class Array
   def uniq?
-    self == self.uniq
+    # at first it was just 'self == uniq' but I wanted to make it more 'mine' instead
+    # of relying on function built in standard ruby library
+    number_counts = Hash.new
+
+    self.each do |number|
+      if number_counts[number]
+        return false
+      else
+        number_counts[number] = 1
+      end
+    end
+
+    true
   end
 end
 
@@ -10,7 +24,7 @@ module FirstApproach
   module_function
 
   def summing_up_numbers(a)
-    until a.uniq? do
+    until a.uniq?
       sum = a.first; a.shift
       unless a.find_index(sum).nil?
         a.delete_at(a.find_index(sum))
@@ -34,3 +48,4 @@ a = []
 n.times { a << gets.chomp.to_i }
 
 puts "First approach gives length of: #{FirstApproach.summing_up_numbers(a)}."
+# puts "Second approach gives length of: #{SecondApproach.summing_up_numbers(a)}."
---
+++ zestaw_zadan_nr_1/obligatory_exercises/summed_up_numbers/summed_up_numbers_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require_relative 'summed_up_numbers'
+
+describe FirstApproach do
+  let(:first_arr) { [2, 3, 4, 4, 5, 1, 3, 2, 5, 4] }
+  let(:second_arr) { [1, 2, 3] }
+  describe '#summing_up_numbers' do
+    it { expect(FirstApproach.summing_up_numbers(first_arr)).to eq(4) }
+    it { expect(FirstApproach.summing_up_numbers(second_arr)).to eq(3) }
+  end
+end
+
+# describe SecondApproach do
+#   let(:first_arr) { [2, 3, 4, 4, 5, 1, 3, 2, 5, 4] }
+#   let(:second_arr) { [1, 2, 3] }
+#   describe '#summing_up_numbers' do
+#     it { expect(SecondApproach.summing_up_numbers(first_arr)).to eq(4) }
+#     it { expect(SecondApproach.summing_up_numbers(second_arr)).to eq(3) }
+#   end
+# end
