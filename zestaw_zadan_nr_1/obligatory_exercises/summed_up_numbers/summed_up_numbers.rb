7576f1b6bd02606ed47ce7705e464c499f542734
N0tR1CH <damianrichter58@gmail.com>
2022-10-15 19:25:28 +0200

    add second approach which uses recursion

--- zestaw_zadan_nr_1/obligatory_exercises/summed_up_numbers/summed_up_numbers.rb
+++ zestaw_zadan_nr_1/obligatory_exercises/summed_up_numbers/summed_up_numbers.rb
@@ -5,14 +5,12 @@ class Array
   def uniq?
     # at first it was just 'self == uniq' but I wanted to make it more 'mine' instead
     # of relying on function built in standard ruby library
-    number_counts = Hash.new
+    number_counts = {}
 
-    self.each do |number|
-      if number_counts[number]
-        return false
-      else
-        number_counts[number] = 1
-      end
+    each do |number|
+      return false if number_counts[number]
+
+      number_counts[number] = 1 unless number_counts[number]
     end
 
     true
@@ -21,24 +19,41 @@ end
 
 module FirstApproach
   # module_function makes method self
+
   module_function
 
   def summing_up_numbers(a)
     until a.uniq?
-      sum = a.first; a.shift
+      sum = a.first
+      a.shift
       unless a.find_index(sum).nil?
         a.delete_at(a.find_index(sum))
-        sum += sum; a.push(sum)
-        next
+        sum += sum
       end
-
       a.push(sum)
-
-      next
     end
 
     # returning an array size
     a.size
+  end
+end
+
+module SecondApproach
+  module_function
+
+  def summing_up_numbers(a)
+    if a.uniq?
+      a.size
+    else
+      sum = a.first
+      a.shift
+      unless a.find_index(sum).nil?
+        a.delete_at(a.find_index(sum))
+        sum += sum
+      end
+      a.push(sum)
+      summing_up_numbers(a)
+    end
   end
 end
 
@@ -47,5 +62,5 @@ a = []
 
 n.times { a << gets.chomp.to_i }
 
-puts "First approach gives length of: #{FirstApproach.summing_up_numbers(a)}."
-# puts "Second approach gives length of: #{SecondApproach.summing_up_numbers(a)}."
+# puts "First approach gives length of: #{FirstApproach.summing_up_numbers(a)}."
+puts "Second approach gives length of: #{SecondApproach.summing_up_numbers(a)}."
--- zestaw_zadan_nr_1/obligatory_exercises/summed_up_numbers/summed_up_numbers_spec.rb
+++ zestaw_zadan_nr_1/obligatory_exercises/summed_up_numbers/summed_up_numbers_spec.rb
@@ -10,11 +10,11 @@ describe FirstApproach do
   end
 end
 
-# describe SecondApproach do
-#   let(:first_arr) { [2, 3, 4, 4, 5, 1, 3, 2, 5, 4] }
-#   let(:second_arr) { [1, 2, 3] }
-#   describe '#summing_up_numbers' do
-#     it { expect(SecondApproach.summing_up_numbers(first_arr)).to eq(4) }
-#     it { expect(SecondApproach.summing_up_numbers(second_arr)).to eq(3) }
-#   end
-# end
+describe SecondApproach do
+  let(:first_arr) { [2, 3, 4, 4, 5, 1, 3, 2, 5, 4] }
+  let(:second_arr) { [1, 2, 3] }
+  describe '#summing_up_numbers' do
+    it { expect(SecondApproach.summing_up_numbers(first_arr)).to eq(4) }
+    it { expect(SecondApproach.summing_up_numbers(second_arr)).to eq(3) }
+  end
+end
