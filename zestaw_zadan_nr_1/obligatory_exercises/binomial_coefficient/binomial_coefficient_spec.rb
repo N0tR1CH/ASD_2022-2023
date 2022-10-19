5cdbd123d1bfddd79eb73e26dce70dcc6de6c895
n0tR1CH <damainrichter58@gmail.com>
2022-10-19 22:37:18 +0200

    add zestaw_zadan_nr_2

--- zestaw_zadan_nr_1/obligatory_exercises/binomial_coefficient/binomial_coefficient_spec.rb
+++ zestaw_zadan_nr_1/obligatory_exercises/binomial_coefficient/binomial_coefficient_spec.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 require 'rspec'
 require_relative 'binomial_coefficient'
 
 RSpec.describe BinomialCoefficient do
-  let(:dummy_class) { Class.new { extend BinomialCoefficient }}
+  let(:dummy_class) { Class.new { extend BinomialCoefficient } }
 
   describe '#recursive' do
     it { expect(dummy_class.recursive(1, 0)).to eq(1) }
     it { expect(dummy_class.recursive(1, 1)).to eq(1) }
     it { expect(dummy_class.recursive(10, 2)).to eq(45) }
-    it { expect(dummy_class.recursive(100, 3)).to eq(161700) }
+    it { expect(dummy_class.recursive(100, 3)).to eq(161_700) }
   end
 
   describe '#dynamic_programming' do
     it { expect(dummy_class.dynamic_programming(1, 0)).to eq(1) }
     it { expect(dummy_class.dynamic_programming(1, 1)).to eq(1) }
     it { expect(dummy_class.dynamic_programming(10, 2)).to eq(45) }
-    it { expect(dummy_class.dynamic_programming(100, 3)).to eq(161700) }
-    it { expect(dummy_class.dynamic_programming(1000, 4)).to eq(41417124750) }
+    it { expect(dummy_class.dynamic_programming(100, 3)).to eq(161_700) }
+    it { expect(dummy_class.dynamic_programming(1000, 4)).to eq(41_417_124_750) }
   end
 end
---
+++ zestaw_zadan_nr_2/Optymalne Dostawy/optymalne_dostawy.rb
@@ -0,0 +1,12 @@
+def optymalne_dostawy(arr)
+
+end
+
+arr = []
+
+liczba_stacji = gets.chomp.to_i
+
+liczba_stacji.times { arr << gets.chomp.to_i }
+
+# showing the array
+p arr
