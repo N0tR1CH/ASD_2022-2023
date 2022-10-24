c3c4edbc113bf9ec3926395090308e803114b421
N0tR1CH <damianrichter58@gmail.com>
2022-10-24 23:54:11 +0200

    add distance between points method

--- zestaw_zadan_nr_2/Gdzie rozwiesic hamak?/hamak.rb
+++ zestaw_zadan_nr_2/Gdzie rozwiesic hamak?/hamak.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Punkt
 class Point
   attr_accessor :x, :y
 
@@ -13,12 +14,17 @@ class Point
   end
 end
 
-arr = [Point.new(4, 4),
-       Point.new(0, 0),
-       Point.new(3, 3),
-       Point.new(3, 1),
-       Point.new(1, 1)]
+# dystans pomiędzy dwoma punktami
+def distance_between_points(point1, point2)
+  Math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
+end
 
-arr.sort_by!(&:x)
+n = 5
 
-puts arr
+coordinates = [Point.new(4, 4),
+               Point.new(0, 0),
+               Point.new(3, 3),
+               Point.new(3, 1),
+               Point.new(1, 1)]
+
+coordinates.sort_by!(&:x)
