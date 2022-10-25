025e473bc0ddcffce0a0adb318364fa24a428a31
n0tR1CH <damainrichter58@gmail.com>
2022-10-25 11:14:00 +0200

    finish optymalne_dostawy.rb without the complexity

--- zestaw_zadan_nr_2/Optymalne Dostawy/optymalne_dostawy.rb
+++ zestaw_zadan_nr_2/Optymalne Dostawy/optymalne_dostawy.rb
@@ -1,9 +1,42 @@
-station_distances = Hash.new
+# frozen_string_literal: true
 
-n = 3
+def qsort(arr)
+  return arr if arr.size < 2
 
-(n.times).each { |i| station_distances[i] = gets.chomp.to_i }
+  pivot = arr.first
+  arr.delete(arr.first)
 
-p station_distances
+  arr_less_than_pivot = arr.select { |element| element < pivot }
+  arr_greater_than_pivot = arr.select { |element| element > pivot }
 
-station_distances.sort_by { |key, value| value }.to_h
+  qsort(arr_less_than_pivot) + [pivot] + qsort(arr_greater_than_pivot)
+end
+
+# hash to store values of distances with its indexes
+hash = {}
+
+# variable to store number of stations
+liczba_stacji = gets.chomp.to_i
+
+# loop to get values into the hash
+liczba_stacji.times.each { |i| hash[i] = gets.chomp.to_i }
+
+# getting sorted array form hash values
+arr = qsort(hash.values)
+
+# getting the median
+median = arr[liczba_stacji / 2]
+
+# deleting median
+arr.delete(median)
+
+# counting sum of differences
+sum = 0
+arr.each { |element| sum += (element - median).abs }
+
+# getting element index
+indeks_elementu = hash.key(median)
+
+# result
+puts indeks_elementu
+puts sum
